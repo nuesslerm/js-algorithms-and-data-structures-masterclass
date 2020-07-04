0ccaf445425f24e9d004415e1eabae67edc5bb4f
nuesslerm <markus@choco.com>
2020-07-04 20:47:33 +0200

    setup jest --watch and nodemon

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { sum, multiply } from './arithmetic/arithmetic.methods';
 
+console.log('----------------------------------------------------------------');
+
 console.log('sum: ', sum(2, 3));
 console.log('multiply: ', multiply(2, 3));
+
+console.log('----------------------------------------------------------------');
