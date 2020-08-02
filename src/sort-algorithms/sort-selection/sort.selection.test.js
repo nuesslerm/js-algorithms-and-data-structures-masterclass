1589f49d5d08912191724cbae041b0aad45470f2
nuesslerm <markus@choco.com>
2020-08-02 19:12:18 +0200

    insertion sort

---
+++ src/sort-algorithms/sort-selection/sort.selection.test.js
@@ -0,0 +1,12 @@
+import { sum, multiply } from './arithmetic.methods';
+
+describe('addition and multiplication', () => {
+  // test sum
+  it('adds 2 + 3 to equal 5', () => {
+    expect(sum(2, 3)).toBe(5);
+  });
+  // test multiply
+  it('multiplies 2 + 3 to equal 6', () => {
+    expect(multiply(2, 3)).toBe(6);
+  });
+});
