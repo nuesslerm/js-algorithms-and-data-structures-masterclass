5190d128ef69b812a5bb51c9ad5973acd69fb76d
nuesslerm <markus@choco.com>
2020-08-15 21:28:47 +0200

    added eslint, prettier, husky, lint-staged 2

--- src/recursion/callstack_example.js
+++ src/recursion/callstack_example.js
@@ -2,15 +2,16 @@ function takeShower() {
   return 'Showering!';
 }
 
+function cookFood() {
+  const items = ['Oatmeal', 'Eggs', 'Protein Shake'];
+  return items[Math.floor(Math.random() * items.length)];
+}
+
 function eatBreakfast() {
-  let meal = cookFood();
+  const meal = cookFood();
   return `Eating ${meal}`;
 }
 
-function cookFood() {
-  let items = ['Oatmeal', 'Eggs', 'Protein Shake'];
-  return items[Math.floor(Math.random() * items.length)];
-}
 function wakeUp() {
   takeShower();
   eatBreakfast();
