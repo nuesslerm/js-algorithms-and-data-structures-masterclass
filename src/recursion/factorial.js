b2677d9924d7d964901e4eea31b9f68fe4fc80f7
nuesslerm <markus@choco.com>
2020-08-16 00:03:16 +0200

    recursive functions part 1

--- src/recursion/factorial.js
+++ src/recursion/factorial.js
@@ -1,3 +1,16 @@
-// const factorial = (num) => {
+// const factorialIter = (num) => {
 //   let total = 1;
+//   for (let i = num; i > 1; i--) {
+//     total *= i;
+//   }
+//   return total;
 // };
+
+// console.log(factorialIter(12));
+
+const factorialRec = (num) => {
+  if (num === 1) return 1;
+  return num * factorialRec(num - 1);
+};
+
+console.log(factorialRec(12));
