55e94db1f4c0f6a3d2444183d038585da72b342a
nuesslerm <markus@choco.com>
2020-08-16 00:55:53 +0200

    recursive functions part 3

--- src/recursion/recursive-helper-functions.js
+++ src/recursion/recursive-helper-functions.js
@@ -1,70 +1,94 @@
 // helper method recursion
 
-const collectOdds = (nums) => {
-  const resArr = [];
+// const collectOdds = (nums) => {
+//   const resArr = [];
 
-  const pushToResArr = (arr) => {
-    if (arr.length === 0) return;
+//   const pushToResArr = (arr) => {
+//     if (arr.length === 0) return;
 
-    /**
-     * var somevar = new String('somestring') console.log(typeof somevar) // object
-     * string type checking ->
-     * if (typeof myVar === 'string' || myVar instanceof String)
-     * // it's a string
-     * else
-     * // it's something else
-     */
-    if (arr[0] % 2 === 1 /* && typeof arr[0] === 'number' */) {
-      resArr.push(arr[0]);
+//     /**
+//      * var somevar = new String('somestring') console.log(typeof somevar) // object
+//      * string type checking ->
+//      * if (typeof myVar === 'string' || myVar instanceof String)
+//      * // it's a string
+//      * else
+//      * // it's something else
+//      */
+//     if (arr[0] % 2 === 1 /* && typeof arr[0] === 'number' */) {
+//       resArr.push(arr[0]);
+//     }
+
+//     pushToResArr(arr.slice(1));
+//   };
+
+//   pushToResArr(nums);
+
+//   return resArr;
+// };
+
+// console.log(typeof ('23434' % 2));
+// console.log(typeof '23434');
+
+// console.log(
+//   collectOdds([
+//     2,
+//     34,
+//     '23',
+//     'sdfdfdf',
+//     '35sdf',
+//     341,
+//     '345',
+//     '624',
+//     34,
+//     64,
+//     634,
+//     63456,
+//     345,
+//     745,
+//     23,
+//     '456',
+//     457,
+//     4,
+//     56,
+//     24,
+//     634,
+//     6,
+//     457,
+//     46,
+//     45,
+//     7455,
+//     645,
+//     64,
+//     7,
+//     456,
+//     45,
+//     6,
+//     457,
+//     457,
+//     35,
+//   ])
+// );
+
+// ----------------------------------------------------------------
+
+function collectOddValues(arr) {
+  const result = [];
+
+  function helper(helperInput) {
+    if (helperInput.length === 0) {
+      return;
     }
 
-    pushToResArr(arr.slice(1));
-  };
+    if (helperInput[0] % 2 !== 0) {
+      result.push(helperInput[0]);
+    }
 
-  pushToResArr(nums);
+    helper(helperInput.slice(1));
+  }
 
-  return resArr;
-};
+  helper(arr);
 
-console.log(typeof ('23434' % 2));
-console.log(typeof '23434');
+  return result;
+}
 
-console.log(
-  collectOdds([
-    2,
-    34,
-    '23',
-    'sdfdfdf',
-    '35sdf',
-    341,
-    '345',
-    '624',
-    34,
-    64,
-    634,
-    63456,
-    345,
-    745,
-    23,
-    '456',
-    457,
-    4,
-    56,
-    24,
-    634,
-    6,
-    457,
-    46,
-    45,
-    7455,
-    645,
-    64,
-    7,
-    456,
-    45,
-    6,
-    457,
-    457,
-    35,
-  ])
-);
+console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
