f92416bd34ec6b22f1f07c45009b36eec227a0cc
nuesslerm <markus@choco.com>
2020-08-16 23:27:18 +0200

    merge sort 4

--- src/sort-algorithms/sort-merge/sort.merge.js
+++ src/sort-algorithms/sort-merge/sort.merge.js
@@ -40,59 +40,73 @@ function merge(arrA, arrB) {
 
 // ------------------------------
 
-function split(arr) {
-  const splitArr = [];
+// function split(arr) {
+//   const splitArr = [];
 
-  function helper(inputArr) {
-    if (inputArr.length === 0 || inputArr.length === 1) {
-      splitArr.push(inputArr);
-      return;
-    }
+//   function helper(inputArr) {
+//     if (inputArr.length === 0 || inputArr.length === 1) {
+//       splitArr.push(inputArr);
+//       return;
+//     }
 
-    const hlength = Math.floor(inputArr.length / 2);
+//     const hlength = Math.floor(inputArr.length / 2);
 
-    const arrA = inputArr.slice(0, hlength);
-    const arrB = inputArr.slice(hlength);
+//     const arrA = inputArr.slice(0, hlength);
+//     const arrB = inputArr.slice(hlength);
 
-    helper(arrA);
-    helper(arrB);
-  }
+//     helper(arrA);
+//     helper(arrB);
+//   }
 
-  helper(arr);
+//   helper(arr);
 
-  return splitArr;
-}
+//   return splitArr;
+// }
 
 // -------------------------------
 
+// function mergeSort(arr) {
+//   const splitArr = split(arr);
+
+//   function helper(inputArr) {
+//     if (inputArr.length === 1) return inputArr[0];
+
+//     const sortedArr = [];
+
+//     let i = 0;
+
+//     while (i < inputArr.length) {
+//       if (!inputArr[i + 1]) {
+//         sortedArr.push(inputArr[i]);
+//       } else {
+//         sortedArr.push(merge(inputArr[i], inputArr[i + 1]));
+//       }
+//       i += 2;
+//     }
+
+//     // console.log(sortedArr);
+//     return helper(sortedArr);
+//   }
+
+//   return helper(splitArr);
+// }
+
 function mergeSort(arr) {
-  const splitArr = split(arr);
+  if (arr.length <= 1) return arr;
 
-  function helper(inputArr) {
-    if (inputArr.length === 1) return inputArr[0];
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
 
-    const sortedArr = [];
-
-    let i = 0;
-
-    while (i < inputArr.length) {
-      if (!inputArr[i + 1]) {
-        sortedArr.push(inputArr[i]);
-      } else {
-        sortedArr.push(merge(inputArr[i], inputArr[i + 1]));
-      }
-      i += 2;
-    }
-
-    // console.log(sortedArr);
-    return helper(sortedArr);
-  }
-
-  return helper(splitArr);
+  return merge(left, right);
 }
 
 const someArr = [2, 6, 233, 7, 8, 11, 34343, 25, 2343];
 
 console.log('mergeSort(someArr)', mergeSort(someArr));
 
-// console.log(someArr[329]);
+// big-O of merge sort
+// time-complexity (best): O(n logn)
+// time-complexity (average): O(n logn)
+// time-complexity (worst): O(n logn)
+// space-complexity: O(n)
