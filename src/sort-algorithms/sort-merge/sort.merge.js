3e5dda89dd855d7a1f26dd47e56b224dd4b07f15
nuesslerm <markus@choco.com>
2020-08-16 23:14:10 +0200

    merge sort 3

--- src/sort-algorithms/sort-merge/sort.merge.js
+++ src/sort-algorithms/sort-merge/sort.merge.js
@@ -8,7 +8,7 @@ function merge(arrA, arrB) {
   let i = 0;
   let j = 0;
 
-  while (i + j < arrA.length + arrB.length) {
+  while (i < arrA.length && j < arrB.length) {
     if (arrA[i] < arrB[j]) {
       mergedArr.push(arrA[i]);
       i += 1;
@@ -18,10 +18,81 @@ function merge(arrA, arrB) {
     }
   }
 
+  while (i < arrA.length) {
+    mergedArr.push(arrA[i]);
+    i += 1;
+  }
+
+  while (j < arrB.length) {
+    mergedArr.push(arrB[j]);
+    j += 1;
+  }
+
   return mergedArr;
 }
 
-const arrA = [1, 3, 4, 5, 9, 123, 224];
-const arrB = [2, 6, 7, 8, 11, 25, 23325];
+// const arrB = [1, 3, 4, 5, 9, 123, 224];
+// const arrA = [2, 6, 7, 8, 11, 25, 233, 2343, 34343];
 
-console.log(merge(arrA, arrB));
+// console.log(merge(arrA, arrB));
+// console.log(arrA[8] > 2);
+// console.log([].length);
+
+// ------------------------------
+
+function split(arr) {
+  const splitArr = [];
+
+  function helper(inputArr) {
+    if (inputArr.length === 0 || inputArr.length === 1) {
+      splitArr.push(inputArr);
+      return;
+    }
+
+    const hlength = Math.floor(inputArr.length / 2);
+
+    const arrA = inputArr.slice(0, hlength);
+    const arrB = inputArr.slice(hlength);
+
+    helper(arrA);
+    helper(arrB);
+  }
+
+  helper(arr);
+
+  return splitArr;
+}
+
+// -------------------------------
+
+function mergeSort(arr) {
+  const splitArr = split(arr);
+
+  function helper(inputArr) {
+    if (inputArr.length === 1) return inputArr[0];
+
+    const sortedArr = [];
+
+    let i = 0;
+
+    while (i < inputArr.length) {
+      if (!inputArr[i + 1]) {
+        sortedArr.push(inputArr[i]);
+      } else {
+        sortedArr.push(merge(inputArr[i], inputArr[i + 1]));
+      }
+      i += 2;
+    }
+
+    // console.log(sortedArr);
+    return helper(sortedArr);
+  }
+
+  return helper(splitArr);
+}
+
+const someArr = [2, 6, 233, 7, 8, 11, 34343, 25, 2343];
+
+console.log('mergeSort(someArr)', mergeSort(someArr));
+
+// console.log(someArr[329]);
